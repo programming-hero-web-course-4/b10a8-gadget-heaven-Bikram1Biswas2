ad3a9c46dc8c54fb606b5077c5400179d373c192
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-03 21:30:05 +0530

    NavBar Done

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-red-500 text-4xl">404 not found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,32 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Home from "./Components/Home/Home";
+import Statistics from "./Components/Statistics/Statistics";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:"/",
+       element:<Home></Home>
+      },
+      {
+        path:"statistics",
+        element:<Statistics></Statistics>
+      },
+      {
+        path:"dashboard",
+        element:<Dashboard></Dashboard>
+      }
+
+    ]
   },
 ]);
 
